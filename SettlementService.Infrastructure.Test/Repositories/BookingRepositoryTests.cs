6cfb4adcdb412e88f1f968a7a73d1efa5be29ba6
Julian Delgado <juliandelgado@alu.frt.edu.ar>
2024-08-09 16:56:25 +1000

    Adding GetAll Test failing

--- SettlementService.Infrastructure.Test/Repositories/BookingRepositoryTests.cs
+++ SettlementService.Infrastructure.Test/Repositories/BookingRepositoryTests.cs
@@ -12,16 +12,12 @@ namespace SettlementService.Infrastructure.Test.Repositories
 {
     public class BookingRepositoryTests
     {
-        private readonly DataContext _context;
-        private readonly BookingRepository _repository;
+        private DataContext _context;
+        private BookingRepository _repository;
 
-        [OneTimeTearDown]
-        public void DisposeContext() 
-        {
-            _context.Dispose();
-        }
 
-        public BookingRepositoryTests()
+        [SetUp]
+        public void SetUp()
         {
             var options = new DbContextOptionsBuilder<DataContext>()
            .UseInMemoryDatabase(databaseName: "TestDB")
@@ -29,6 +25,25 @@ namespace SettlementService.Infrastructure.Test.Repositories
 
             _context = new DataContext(options);
             _repository = new BookingRepository(_context);
+
+            SeedDatabase();
+        }
+
+        [TearDownAttribute]
+        public void DisposeContext()
+        {
+            _context.Dispose();
+        }
+        private void SeedDatabase()
+        {
+            var bookings = new List<Booking>
+            {
+                new Booking { Id = Guid.NewGuid(), ClientName = "John Doe", BookingTime = new TimeOnly(10,0) },
+                new Booking { Id = Guid.NewGuid(), ClientName = "Jane Doe", BookingTime = new TimeOnly(11,0) }
+            };
+
+            _context.Bookings.AddRange(bookings);
+            _context.SaveChanges();
         }
 
         [Test]
@@ -40,6 +55,27 @@ namespace SettlementService.Infrastructure.Test.Repositories
 
             Assert.AreNotEqual(expected: Guid.Empty, actual: newBookingId);
             Assert.NotNull(newBooking);
+        }
+
+        [Test]
+        public async Task Create_ShouldAddANewEntityToTheDatabaseWithCorrectValues()
+        {
+            Booking booking = new Booking { Id = Guid.NewGuid(), ClientName = "Julian Delgado", BookingTime = new TimeOnly(12,0) };
+            Guid newBookingId = await _repository.CreateAsync(booking);
+            Booking newBooking = await _context.Bookings.FindAsync(newBookingId);
+
+            Assert.AreEqual(expected: booking.Id, actual: newBooking.Id);
+            Assert.AreEqual(expected: booking.ClientName, actual: newBooking.ClientName);
+            Assert.AreEqual(expected: booking.BookingTime, actual: newBooking.BookingTime);
+        }
+
+        [Test]
+        public async Task GetAll_ShouldBringAllBookings()
+        {
+            //override bookings from context to test
+            List<Booking> bookings = await _repository.GetAllAsync();
+            Assert.NotNull(bookings);
+            Assert.IsTrue(bookings.Count > 0);
         }
     }
 }
