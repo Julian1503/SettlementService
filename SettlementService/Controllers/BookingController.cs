11393ddc6306a97a1260b1957d1ff7d412e33294
Julian Delgado <juliandelgado@alu.frt.edu.ar>
2024-08-11 22:43:10 +1000

    Adding changes to the controller and a middleware to handle exceptions

--- SettlementService/Controllers/BookingController.cs
+++ SettlementService/Controllers/BookingController.cs
@@ -1,34 +1,54 @@
-﻿using AutoMapper;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using SettlementService.API.AutoMapperDto;
-using SettlementService.API.Dto;
+﻿using Microsoft.AspNetCore.Mvc;
+using SettlementService.API.Response.Booking;
+using SettlementService.Domain.Abstractions;
+using SettlementService.DTO.Booking;
 using SettlementService.Interfaces.Booking;
-using SettlementService.Interfaces.Model;
-using SettlementService.Mapper;
 
 namespace SettlementService.API.Controllers
 {
+    /// <summary>
+    /// API controller for managing bookings.
+    /// </summary>
+    
     [Route("api/[controller]")]
     [ApiController]
     public class BookingController : ControllerBase
     {
-        private IBookingService _bookingService;
-        private IMapper _mapper;
+        private readonly IBookingService _bookingService;
 
         public BookingController(IBookingService bookingService)
         {
             _bookingService = bookingService;
-            MapperConfiguration config = new MapperConfiguration(cfg => cfg.AddProfile<ProfileDtoConfig>());
-            _mapper = config.CreateMapper();
         }
 
-        [HttpPost("AddBooking")]
-        public async Task<IActionResult> AddBooking(BookingDto bookingDto)
+        /// <summary>
+        /// Creates a new booking based on the provided booking data.
+        /// </summary>
+        /// <param name="bookingDto">The booking data transfer object containing booking details.</param>
+        /// <returns>An action result indicating the outcome of the booking creation.</returns>
+        
+        [HttpPost]
+        public async Task<IActionResult> AddNewBooking(BookingDto bookingDto)
         {
-            BookingModel bookingModel = _mapper.Map<BookingModel>(bookingDto);
-            Guid newBookingId = await _bookingService.AddNewBooking(bookingModel);
-            return Ok(newBookingId);
+            // Call the booking service to add a new booking
+            Result<Guid> result = await _bookingService.AddNewBooking(bookingDto);
+
+            // Check if the booking was successfully created
+            if (result.isSuccess)
+            {
+                // Return a successful response with the new booking ID
+                BookingAddResponse response = new BookingAddResponse(result.Value);
+                return Ok(response);
+            }
+            else
+            {
+                // Return a problem response with error details
+                return Problem(
+                    title: result.Error.Title,
+                    detail: result.Error.Detail,
+                    statusCode: result.Error.Status
+                );
+            }
         }
     }
 }
